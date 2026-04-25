eae04028144653cf393cfb95a6a0245ec21288c2
frazgul507-afk <frazgul507@gmail.com>
2026-04-25 15:58:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("School System Running");
+let students = JSON.parse(localStorage.getItem("students")) || [];
+
+function saveData() {
+  localStorage.setItem("students", JSON.stringify(students));
+}
+
+function addStudent() {
+  let name = document.querySelector("input[placeholder='Student Name']").value;
+  let cls = document.querySelector("input[placeholder='Class']").value;
+  let fee = document.querySelector("input[placeholder='Monthly Fee']").value;
+
+  if(name === "" || cls === "" || fee === "") {
+    alert("Please fill all fields");
+    return;
+  }
+
+  students.push({
+    name: name,
+    cls: cls,
+    fee: parseInt(fee),
+    paid: 0
+  });
+
+  saveData();
+  alert("Student Added Successfully ✔");
+  location.reload();
+}
